9b087cc4bb8b98feffdbac1e3550324c5b1b6f35
Marhs2 <165652450+Marhs2@users.noreply.github.com>
2025-11-17 20:16:51 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.math.BigInteger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if (a == 0 && b == 0) break;
+            System.out.println(a + b);
+
+        }
+
+
+    }
+}
