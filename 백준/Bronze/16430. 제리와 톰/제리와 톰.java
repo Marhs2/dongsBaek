c36aac38a81abcf6eb812ca5716bff21e44569ee
Marhs2 <165652450+Marhs2@users.noreply.github.com>
2025-11-18 19:57:29 +0900

    [Bronze V] Title: 제리와 톰, Time: 168 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.math.BigInteger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d %d", b- (a % b), b);
+
+
+    }
+}
