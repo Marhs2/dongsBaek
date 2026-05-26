5032009e230ca6d7351f13920a8afe61ba5ac3a8
Marhs2 <165652450+Marhs2@users.noreply.github.com>
2026-05-26 19:11:42 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 5.01 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -0,0 +1,46 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+class Solution {
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm:ss");
+
+        LocalTime videoLen = LocalTime.parse("00:" + video_len);
+        LocalTime startTime = LocalTime.parse("00:" + op_start);
+        LocalTime endTime = LocalTime.parse("00:" + op_end);
+        LocalTime posTime = LocalTime.parse("00:" + pos);
+        if ((startTime.isBefore(posTime) || startTime.equals(posTime)) && (endTime.isAfter(posTime) || endTime.equals(posTime)))
+            posTime = endTime;
+
+        for (String ch : commands) {
+
+
+            if (ch.equals("next")) {
+                if (posTime.plusSeconds(10).isAfter(videoLen)) {
+                    posTime = videoLen;
+                } else {
+                    posTime = posTime.plusSeconds(10);
+
+                }
+            } else {
+                if (posTime.isBefore(LocalTime.of(0, 0, 10))) {
+                    posTime = LocalTime.of(0, 0);
+                } else {
+                    posTime = posTime.minusSeconds(10);
+
+                }
+            }
+
+            if (!posTime.isBefore(startTime) && !posTime.isAfter(endTime)) {
+                posTime = endTime;
+            }
+
+
+        }
+
+
+        return posTime.format(formatter);
+    }
+}
